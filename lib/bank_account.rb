2c014d5bda00f29220b3d68f6eb6e2c2919965fc
milanla <milan.lanlada@gmail.com>
2018-09-20 16:30:11 -0400

    Transfered that moneyyyy

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class BankAccount
+
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name, balance=1000, status="open")
+    @name = name
+    @balance = balance
+    @status = status
+  end
+
+  def deposit(deposit)
+    @balance += deposit
+  end
+
+  milan = BankAccount.new("milan")
+
+  def display_balance
+    return "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+
+  def close_account
+    self.status= "closed"
+  end
 
 end
